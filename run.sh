db21289ee85abcc8840490066de1a83fcb939ad9
Ankit Kumar <70105136+ankit435@users.noreply.github.com>
2021-07-06 09:19:01 +0000

    correct configuration

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 source=$1
 
 # echo "\033[0;33m running g++ ${source} \033[0m\n"
-echo "--> running g++ ${source} <--\n"
+echo "             --> running g++ ${source} <--"
+echo ""
 
 g++ $source && ./a.out
